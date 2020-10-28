eb5596a80cb3f696eaea3f519d445f8ac3da5515
rohit18259 <73587796+rohit18259@users.noreply.github.com>
2020-10-28 16:19:46 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,35 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Rohit Roy
+   Roll_Number: 2018259 */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int a = 10;
+
+int main(){
+	pid_t pid;
+	pid = fork();
+	if (pid<0){
+		fprintf(stderr,"fork failed");
+		return 1;
+	}
+	else if (pid==0){
+		while (a>-90){
+			printf("child process: %d\n",a);
+			a--;
+		}
+		printf("child process: %d\n",a);
+		printf("child process over\n");
+	}
+	else{
+		while (a<100){
+			printf("parent process: %d\n",a);
+			a++;
+		}
+		printf("parent process: %d\n",a);
+		printf("parent process over\n");
+		waitpid(-1,NULL,0);
+	}
+	return 0;
+}
